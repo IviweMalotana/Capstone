b381b312dfaa53c6265e59be4b5cb73a609cfb69
IviweMalotana <53525294+IviweMalotana@users.noreply.github.com>
2022-03-25 08:30:58 +0200

    Power Automate

---
+++ Shared/Filters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Shared
+{
+    public class Filters
+    {
+        public bool Dates { get; set; }
+        public bool Projects { get; set; }
+        public bool ResourceGroups { get; set; }
+        public bool Subscriptions { get; set; }
+        public bool Vendors { get; set; }
+    }
+}
